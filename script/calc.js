70cd080136dc294da4674b3e84d8a4f5c7ff40e5
Marcos-Gregorio <marcosgregs.frontend@gmail.com>
2023-03-08 18:17:59 -0300

    C10K-ResponsiDesingInit

--- script/calc.js
+++ script/calc.js
@@ -1,15 +1,31 @@
-var form = document.querySelector('form');
-var resultDiv = document.getElementById('result');
+var formMotor = document.getElementById('formMotor');
+var resultDiv = document.getElementById('resultMotor');
 
-form.addEventListener('submit', function (event) {
+var formCar = document.getElementById('formCar');
+var resultCar = document.getElementById('resultCar');
+
+
+formMotor.addEventListener('submit', function (event) {
   event.preventDefault();
-
-  var origin = document.getElementById('origin').value;
+  
+  var origin ='Rua França Meireles';
   var destination = document.getElementById('destination').value;
 
   calcDistance(origin, destination, function (distance) {
-    var shippingCost = distance * 1.5; // 1.5 reais por km
+    var shippingCost = distance * 2.5; // 1.5 reais por km
     resultDiv.innerHTML = 'Distância: ' + distance.toFixed(2) + ' km<br>Valor do frete: R$' + shippingCost.toFixed(2);
+  });
+});
+
+formCar.addEventListener('submit', function (event) {
+  event.preventDefault();
+  
+  var origin ='Rua França Meireles';
+  var destination = document.getElementById('destinationCar').value;
+
+  calcDistance(origin, destination, function (distance) {
+    var shippingCost = distance * 3.5; // 1.5 reais por km
+    resultCar.innerHTML = 'Distância: ' + distance.toFixed(2) + ' km<br>Valor do frete: R$' + shippingCost.toFixed(2);
   });
 });
 
